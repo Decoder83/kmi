37f2c3506b473a4d4121ecba5f38d6f14b74b1c4
Decoder83 <xxxx43210@gmail.com>
2024-03-26 19:47:34 +0200

    123

---
+++ js/lang_lt.js
@@ -0,0 +1,14 @@
+const lang_lt = {
+   title: 'Kūno masės indeksas',
+   description: 'Kūno masės indekso (KMI) skaičiuoklė gali būti naudojama KMI reikšmei apskaičiuoti ir atitinkamai svorio būsenai nustatyti.',
+   heightLabel: 'Įveskite savo aukštį cm',
+   weightLabel: 'Įveskite savo svorį kg',
+   calculateBtn: 'Apskaičiuoti',
+   clearBtn: 'Išvalyti',
+   startCalculating: 'Pradėkite skaičiuoti!',
+   enterBothValues: 'Įveskite tiek savo ūgį, tiek svorį!',
+   underweightMessage: 'Jūsų KMI yra mažesnis nei vidutinis ',
+   normalMessage: 'Sveikiname, jūsų KMI yra normalus ',
+   overweightMessage: 'Jūsų KMI yra didesnis nei vidutinis ',
+   aboveNormalMessage: 'Jūsų KMI yra didesnis už normalų ',
+};
